d5b6d56f81fc77728481aa3ff0c35334000c185b
Paul Upendo <paulupendo@cloud.upwork.com>
2020-02-18 18:57:51 +0300

    chore: implement PR feedback

--- src/components/svg-object/svg-object.component.jsx
+++ src/components/svg-object/svg-object.component.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import './svg-object.styles.scss';
+import './svg-object.scss';
 
 const SvgObject = () => (
   <div className='wrapper'>
-    <div className='upper'>
+    <div className='upper pointer'>
       <img className='upper-image' src='/assets/images/upper.svg' alt='upper-shell' />
     </div>
 
-    <div>
+    <div className='pointer'>
       <img src='assets/images/bottom.svg' alt='bottom-shell' />
     </div>
   </div>
